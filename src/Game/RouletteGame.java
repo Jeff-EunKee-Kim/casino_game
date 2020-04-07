f6b379a585d54072129171ea60e034bd23d50e38
Celine Murugi <celine.murugi@duke.edu>
2020-04-07 21:10:45 +0300

    Added rules for the Roulette game

---
+++ src/Game/RouletteGame.java
@@ -0,0 +1,63 @@
+package Game;
+
+import Casino.RandomValueSelector;
+import View.FrontEnd;
+
+public class RouletteGame {
+    public static final int WIN_BLACK_OR_RED = 200;
+    public static final int WIN_SINGLE_NUMBER = 500;
+    public static final int LOSE_BY_LOSS = 100;
+
+
+    private int BET_BY_PLAYER;
+    private int MACHINE_CHOSEN_VALUE;
+    private int CURRENT_MONEY;
+
+
+    public RouletteGame() { // 0 and -1 = Black and Red Bets respectively; 1-36 = Single Number Bets
+        FrontEnd frontEnd = new FrontEnd();
+        BET_BY_PLAYER = frontEnd.betByPlayer(); // a number between -1 and 36
+        CURRENT_MONEY= frontEnd.getCurrentMoney(); // gets the amount of money displayed on screen
+
+        RandomValueSelector randomValueSelector = new RandomValueSelector();
+        MACHINE_CHOSEN_VALUE = randomValueSelector.chosenValue(); // from the casino module
+    }
+
+
+
+    private void checkIfWon(){
+        if (playerHasWon() == true) {
+            addMoneyToPlayer();
+
+        }else{
+            subtractMoneyFromPlayer();
+        }
+    }
+
+
+
+    private boolean playerHasWon(){
+        return (BET_BY_PLAYER == MACHINE_CHOSEN_VALUE);
+    }
+
+
+
+    private void addMoneyToPlayer(){
+        if (BET_BY_PLAYER == 0 || BET_BY_PLAYER == -1){
+            CURRENT_MONEY += WIN_BLACK_OR_RED;
+
+        }else{
+            CURRENT_MONEY += WIN_SINGLE_NUMBER;
+        }
+    }
+
+
+    private void subtractMoneyFromPlayer(){
+        CURRENT_MONEY -= LOSE_BY_LOSS;
+    }
+
+
+    public int getCurrentMoney(){
+        return CURRENT_MONEY;
+    }
+}
