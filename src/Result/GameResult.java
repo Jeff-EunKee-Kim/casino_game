47f2912a7490abae674246230090fc6f7026f1eb
Jeff Kim <eunkeekim@gmail.com>
2020-04-24 01:03:28 +0900

    updated result

--- src/Result/GameResult.java
+++ src/Result/GameResult.java
@@ -7,9 +7,11 @@ package Result;
 public abstract class GameResult {
     private boolean winStatus;
     private int amountWon;
-    public GameResult(boolean winStatus, int amountWon) {
+    private int playerBalance;
+    public GameResult(boolean winStatus, int amountWon, int playerBalance) {
         this.winStatus = winStatus;
         this.amountWon = amountWon;
+        this.playerBalance = playerBalance;
     }
 
     public boolean getWinStatus() {
@@ -19,5 +21,7 @@ public abstract class GameResult {
     public int getAmountWon() {
         return amountWon;
     }
+
+    public int getPlayerBalance() { return playerBalance; }
 
 }
--- src/Result/SlotResult.java
+++ src/Result/SlotResult.java
@@ -5,8 +5,15 @@ package Result;
  * started 4/24/20
  */
 public class SlotResult extends GameResult {
+    private int[] icons;
 
     public SlotResult(boolean win, int amountWon) {
         super(win, amountWon);
+        setIcons();
     }
+
+    public void setIcons() {
+
+    }
+
 }
