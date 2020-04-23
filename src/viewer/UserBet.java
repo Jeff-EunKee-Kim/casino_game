4e0859f0bcb2ca5df4b7131736cf51a95312b727
Jeff Kim <eunkeekim@gmail.com>
2020-04-24 00:17:18 +0900

    modified userbet

--- src/viewer/UserBet.java
+++ src/viewer/UserBet.java
@@ -1,13 +1,8 @@
 package viewer;
 
+import java.util.HashMap;
+
+// 0~36 numbers, 37: red, 38: black, 39: odd, 40: even
 public class UserBet {
-
-
-    private Spread[] spreads;
-    private int redBlack;
-    private int oddEven;
-    private int rowSpread;
-    private int colSpread;
-    private boolean isZero;
-
+    private HashMap<Integer, Integer> bettings;
 }
